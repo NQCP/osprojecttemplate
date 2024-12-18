dd7146a83b580b2b7cfa2a15aca08758125ce88c
David Scavnicky <qdc434@ku.dk>
2024-12-18 11:09:13 +0100

    Enhance version retrieval logic to support project directory detection and improve error handling for missing pyproject.toml

--- {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/_version.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/_version.py
@@ -1,12 +1,30 @@
 def _get_version() -> str:
     from pathlib import Path
-
     import versioningit
+    import nqcpdpgitforwiki
 
-    import {{cookiecutter.package_name}}
+    nqcpdpgitforwiki_path = Path(nqcpdpgitforwiki.__file__).parent
+    project_dir = nqcpdpgitforwiki_path.parent
 
-    {{cookiecutter.package_name}}_path = Path({{cookiecutter.package_name}}.__file__).parent
-    return versioningit.get_version(project_dir={{cookiecutter.package_name}}_path.parent)
+    # Check if running from site-packages and it indicates that the package has been installed
+    if "site-packages" in str(project_dir):
+        # Traverse up to find the root project directory
+        for parent in Path(__file__).resolve().parents:
+            if (parent / "pyproject.toml").exists():
+                project_dir = parent
+                break
 
+    if not (project_dir / "pyproject.toml").exists():
+        raise FileNotFoundError("pyproject.toml not found in the project directory")
+
+    print(f"Project directory: {project_dir}")
+
+    version = versioningit.get_version(project_dir=project_dir)
+    print(f"Retrieved version: {version}")
+
+    return version
 
 __version__ = _get_version()
+
+
+""" __version__ = "0.1.1" """
